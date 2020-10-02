a61a2a6669a206077c384f7febf6953763e54ca6
saymanbro <sayman7588@gmail.com>
2020-10-02 15:34:10 -0700

    dotenv set

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 
 
-const uri = 'mongodb+srv://emaJohnWatson:emajohn9900@cluster0.p7276.mongodb.net/ecomarceProduct?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p7276.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("eocmarceProduct").collection("products");
