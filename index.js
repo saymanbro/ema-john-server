dc3a5fa8c714c709d32abaa105d5411a71bad736
saymanbro <sayman7588@gmail.com>
2020-10-02 15:03:39 -0700

    ema-johm-server-code done

--- index.js
+++ index.js
@@ -11,19 +11,43 @@ app.use(cors())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p7276.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = 'mongodb+srv://emaJohnWatson:emajohn9900@cluster0.p7276.mongodb.net/ecomarceProduct?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("eocmarceProduct").collection("products");
+  const productsCollection = client.db("eocmarceProduct").collection("products");
+  const ordersCollection = client.db("eocmarceProduct").collection("orders");
   app.post("/addProduct", (req, res) => {
     const product = req.body 
-    collection.insertOne(product)
-    .then(result => {
-      console.log(result);
+    productsCollection.insertOne(product)
+    .then(result => { 
+     res.send(result.insertedCount)
     })
   })
-  
- 
+
+
+  app.get('/products', (req, res) => {
+    productsCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+
+
+  app.get('/product/:key', (req, res) => {
+    productsCollection.find({key: req.params.key})
+    .toArray((err, documents) => {
+      res.send(documents[0])
+    })
+  })
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body 
+    ordersCollection.insertOne(order)
+    .then(result => { 
+     res.send(result.insertedCount > 0)
+    })
+  })
+
 });
 
 
